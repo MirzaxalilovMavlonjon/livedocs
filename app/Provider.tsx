58576ec2f2875be255af2398d6a2690107b3763d
Mavlonjon Mirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2024-09-24 09:53:49 +0500

    some components update

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -3,7 +3,7 @@ import Loader from '@/components/Loader'
 import React, { createContext } from 'react'
 
 import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react'
-import { getClerckUsers } from '@/lib/actions/user.actions'
+import { getClerkUsers } from '@/lib/actions/user.actions'
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
 	return (
@@ -11,7 +11,7 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
 			<LiveblocksProvider
 				authEndpoint={'/api/liveblocks-auth'}
 				resolveUsers={async ({ userIds }) => {
-					const user = await getClerckUsers({ userIds })
+					const user = await getClerkUsers({ userIds })
 
 					return user;
 				}}
